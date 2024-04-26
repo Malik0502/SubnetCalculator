b8adbe3a10b4ede5ff81bdd2cdc22a9c93f0e8bd
Malik0502 <65547172+Malik0502@users.noreply.github.com>
2024-04-26 10:58:19 +0200

    Aufgabe für Asymmetrisch notiert

--- Subnetzrechner/Program.cs
+++ Subnetzrechner/Program.cs
@@ -4,6 +4,14 @@ namespace Subnetzrechner
 {
     public class Program
     {
+        // Aufgaben für asymmetrischen Subnetzberechner 
+        // Gegeben: Ip-Adresse, Anzahl der Subnetze und Anzahl der Hosts in den Subnetzen (unterschiedlich)
+        // 1. Das kleinst möglich passende finden (Bei 30 Hosts -> 32, Bei 63 Hosts -> 64 etc.)
+        // Davon dann Log (32 -> Log2(32) = 5 etc.)
+        // 2. Subnetzmaske herausfinden
+        // 3. Mit den Informationen dann Berechnung durchführen.
+        // Sollte nicht groß unterschiedlich sein nach den Schritten
+
         static void Main(string[] args)
         {
             try
